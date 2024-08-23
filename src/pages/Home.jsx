342b5068f9d497fc90c8bd8bd7bac069f08b21dc
mrkhan-dev <sk5094625@gmail.com>
2024-08-24 03:03:28 +0600

    update something

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,9 @@ import Categories from "../Components/Categories/Categories";
 const Home = () => {
   return (
     <div className="md:w-[1536px] mx-auto">
-      {/* Banner Section */}
       <div>
         <Banner />
       </div>
-      {/* Categories Section */}
       <div>
         <Categories></Categories>
       </div>
