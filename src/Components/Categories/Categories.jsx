9c499ccb9e0a41ccc522b29ac43eb45a2c8321d6
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-08-24 03:10:57 +0600

    Categories Update

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -8,30 +8,36 @@ import { SiCapacitor } from 'react-icons/si';
 
 const Categories = () => {
     return (
-        <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-5 m-5'>
-            <div className='bg-[#f6f8fa]  p-5 flex flex-col justify-center items-center hover:text-[#00bba6] hover:bg-[#00bba525] duration-150 cursor-pointer'>
-                <IoIosMan className='text-5xl' />
-                <h1 className='text-2xl'>Men Store</h1>
+        <div>
+            <div className='text-center space-y-2 my-5'>
+                <h1 className=' text-3xl'>FEATURED CATEGORIES</h1>
+                <p className='text-2xl'>Get your desired product from featured category</p>
             </div>
-            <div className='bg-[#f6f8fa]  p-5 flex flex-col justify-center items-center hover:text-[#00bba6] hover:bg-[#00bba525] duration-150 cursor-pointer'>
-                <IoWoman className='text-5xl' />
-                <h1 className='text-2xl'>Women Store</h1>
-            </div>
-            <div className='bg-[#f6f8fa]  p-5 flex flex-col justify-center items-center hover:text-[#00bba6] hover:bg-[#00bba525] duration-150 cursor-pointer'>
-                <FaChildren className='text-5xl' />
-                <h1 className='text-2xl'>Children Store</h1>
-            </div>
-            <div className='bg-[#f6f8fa]  p-5 flex flex-col justify-center items-center hover:text-[#00bba6] hover:bg-[#00bba525] duration-150 cursor-pointer'>
-                <SiCapacitor className='text-5xl' />
-                <h1 className='text-2xl'>Accessories Store</h1>
-            </div>
-            <div className='bg-[#f6f8fa]  p-5 flex flex-col justify-center items-center hover:text-[#00bba6] hover:bg-[#00bba525] duration-150 cursor-pointer'>
-                <GiShoebillStork className='text-5xl' />
-                <h1 className='text-2xl'>Footwear Store</h1>
-            </div>
-            <div className='bg-[#f6f8fa]  p-5 flex flex-col justify-center items-center hover:text-[#00bba6] hover:bg-[#00bba525] duration-150 cursor-pointer'>
-                <MdOutlineDeviceHub className='text-5xl' />
-                <h1 className='text-2xl'>Electronic Store</h1>
+            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-5 m-5'>
+                <div className='bg-[#f6f8fa]  text-[#9c9c9c] p-5 flex flex-col justify-center items-center hover:text-[#00bba6] hover:bg-[#00bba525] duration-150 cursor-pointer'>
+                    <IoIosMan className='text-5xl ' />
+                    <h1 className='text-2xl'>Men Store</h1>
+                </div>
+                <div className='bg-[#f6f8fa] text-[#9c9c9c]  p-5 flex flex-col justify-center items-center hover:text-[#00bba6] hover:bg-[#00bba525] duration-150 cursor-pointer'>
+                    <IoWoman className='text-5xl' />
+                    <h1 className='text-2xl'>Women Store</h1>
+                </div>
+                <div className='bg-[#f6f8fa] text-[#9c9c9c]  p-5 flex flex-col justify-center items-center hover:text-[#00bba6] hover:bg-[#00bba525] duration-150 cursor-pointer'>
+                    <FaChildren className='text-5xl' />
+                    <h1 className='text-2xl'>Children Store</h1>
+                </div>
+                <div className='bg-[#f6f8fa] text-[#9c9c9c]  p-5 flex flex-col justify-center items-center hover:text-[#00bba6] hover:bg-[#00bba525] duration-150 cursor-pointer'>
+                    <SiCapacitor className='text-5xl' />
+                    <h1 className='text-2xl'>Accessories Store</h1>
+                </div>
+                <div className='bg-[#f6f8fa] text-[#9c9c9c]  p-5 flex flex-col justify-center items-center hover:text-[#00bba6] hover:bg-[#00bba525] duration-150 cursor-pointer'>
+                    <GiShoebillStork className='text-5xl' />
+                    <h1 className='text-2xl'>Footwear Store</h1>
+                </div>
+                <div className='bg-[#f6f8fa]  text-[#9c9c9c] p-5 flex flex-col justify-center items-center hover:text-[#00bba6] hover:bg-[#00bba525] duration-150 cursor-pointer'>
+                    <MdOutlineDeviceHub className='text-5xl' />
+                    <h1 className='text-2xl'>Electronic Store</h1>
+                </div>
             </div>
         </div>
     );
