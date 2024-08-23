e7c058865d0c3af48905396280897c389e749c54
mrkhan-dev <sk5094625@gmail.com>
2024-08-24 02:48:25 +0600

    install swiper js

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import Categories from '../Components/Categories/Categories';
+import Banner from "../Components/Banner/Banner";
+import Categories from "../Components/Categories/Categories";
 
 const Home = () => {
   return (
-    <div>
-      {/* Bannger Section */}
-        <div>
-
-        </div>
-        {/* Categoresh Section */}
-        <div>
-          
-          <Categories></Categories>
-        </div>
+    <div className="md:w-[1536px] mx-auto">
+      {/* Banner Section */}
+      <div>
+        <Banner />
+      </div>
+      {/* Categories Section */}
+      <div>
+        <Categories></Categories>
+      </div>
     </div>
   );
 };
