60be714ed170c4c2b6dc9c86d147827291714f30
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-08-23 13:49:33 +0600

    Navbar Update

--- src/Layout/Root/Root.jsx
+++ src/Layout/Root/Root.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navbar from '../../Components/Navbar/Navbar';
 
 const Root = () => {
     return (
         <div>
-            <h1>This isfdfdfdfdfddffdf a Root</h1>
+            <Navbar />
             <Outlet></Outlet>
         </div>
     );
