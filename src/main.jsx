5c7ded43f372b3fd6a1a0aa9d25d355235960e5b
mrkhan-dev <sk5094625@gmail.com>
2024-08-24 21:56:27 +0600

    changes design

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './Router/Router.jsx'
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import "./index.css";
+import {RouterProvider} from "react-router-dom";
+import router from "./Router/Router.jsx";
+import {HelmetProvider} from "react-helmet-async";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-     <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  </StrictMode>
+);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
+import {Helmet} from "react-helmet-async";
 import Banner from "../Components/Banner/Banner";
 import Categories from "../Components/Categories/Categories";
-
 
 const Home = () => {
   return (
     <div className="mx-10 my-10">
+      <Helmet>
+        <title>Quick Shop | Home</title>
+      </Helmet>
       <Banner />
       <div>
         <Categories></Categories>
