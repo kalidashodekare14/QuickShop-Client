bbb4e3b27c33ccb3023f36860313bd47655fd029
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-08-24 14:39:36 +0600

    Banner Update for Edit

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -13,7 +13,7 @@ const Categories = () => {
                 <h1 className=' text-3xl'>FEATURED CATEGORIES</h1>
                 <p className='text-2xl'>Get your desired product from featured category</p>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-5 m-5'>
+            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-5 '>
                 <div className='bg-[#f6f8fa]  text-[#9c9c9c] p-5 flex flex-col justify-center items-center hover:text-[#00bba6] hover:bg-[#00bba525] duration-150 cursor-pointer'>
                     <IoIosMan className='text-5xl ' />
                     <h1 className='text-2xl'>Men Store</h1>
