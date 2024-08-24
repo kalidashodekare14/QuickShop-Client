21cac65b88f03d300f03f2235204fdf08744731c
mrkhan-dev <sk5094625@gmail.com>
2024-08-24 20:17:50 +0600

    update something

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -11,8 +11,8 @@ import {Autoplay, Pagination, Navigation} from "swiper/modules";
 
 const Banner = () => {
   return (
-    <div className="flex gap-4">
-      <div className="w-2/3">
+    <div className="md:flex gap-4 mt-4">
+      <div className="md:w-2/3">
         <>
           <Swiper
             spaceBetween={30}
@@ -30,24 +30,29 @@ const Banner = () => {
           >
             <SwiperSlide>
               <img
-                className="w-full h-[520px] bg-cover"
+                className="w-full md:h-[520px] bg-cover"
                 src="https://i.ibb.co/f9P2VrX/woman-wearing-black-hat-sunglasses-poses-photo-1185498-16694.jpg"
                 alt="image"
               />
             </SwiperSlide>
             <SwiperSlide>
               <img
-                className="w-full h-[520px] bg-cover"
-                src="https://i.ibb.co/f9P2VrX/woman-wearing-black-hat-sunglasses-poses-photo-1185498-16694.jpg"
+                className="w-full md:h-[520px] bg-cover"
+                src="https://i.ibb.co/DVhb8mx/stylish-male-model-business-suits-relaxed-fit-fashion-1277187-22649.jpg"
+                alt="image"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className="w-full md:h-[520px] bg-cover"
+                src="https://i.ibb.co/Xj28JH0/little-girl-model-studio-shopping-1303-7461.jpg"
                 alt="image"
               />
             </SwiperSlide>
           </Swiper>
         </>
       </div>
-      <div>
-        <p>hello</p>
-      </div>
+      <div></div>
     </div>
   );
 };
