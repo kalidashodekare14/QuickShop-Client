e5d54c806cc5d39b827253ef3a1b8c78af4ef790
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-08-26 09:20:12 +0600

    Project Shop Page Update

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -8,15 +8,24 @@ import { FaChartBar, FaSearch } from "react-icons/fa";
 const Shop = () => {
 
     const [isOpen, setIsOpen] = useState(false)
+    const [priceRange, setPriceRange] = useState([0, 500])
+    // const [minPrice, setMinPrice] = useState("")
+    // const [maxPrice, setMaxPrice] = useState("")
 
     const toggleSidebar = () => {
         setIsOpen(!isOpen)
     }
 
+
+    const handlePriceChange = (v) => {
+        setPriceRange(v)
+    }
+
+
     return (
         <div className='flex bg-[#f2f4f8] pt-10 relative'>
             {/* Product Filtering */}
-            <div className={`fixed z-10 left-0  transform ${isOpen ? 'translate-x-2 translate-y-16 p-3 border' : '-translate-x-full'} transition-transform duration-300 ease-in-out   md:relative md:translate-x-0 bg-white w-72 min-h-screen space-y-2`}>
+            <div className={`lg:ms-5 fixed z-10 left-0 lg:translate-y-0 translate-y-16 transform ${isOpen ? 'translate-x-2 translate-y-16 p-3 border' : '-translate-x-full'} transition-transform duration-300 ease-in-out   md:relative md:translate-x-0 bg-white w-72 min-h-screen space-y-2`}>
                 <select className="select select-bordered w-full">
                     <option disabled selected>Category</option>
                     <option>Han Solo</option>
@@ -39,7 +48,7 @@ const Shop = () => {
                     <option>Han Solo</option>
                     <option>Greedo</option>
                 </select>
-                <div className=' bg-white w-full  flex flex-col  justify-center'>
+                <div className='bg-white w-full  flex flex-col  justify-center'>
                     <div className="p-3 border-b">
                         <h1 className="">Price Range</h1>
                     </div>
@@ -48,14 +57,16 @@ const Shop = () => {
                             id="range-slider-gradient"
                             className="margin-lg"
                             step={"any"}
+                            defaultValue={priceRange}
+                            onInput={handlePriceChange}
                         />
                     </div>
                     <div className="flex items-center justify-around pb-5">
                         <div className="flex justify-center items-center border w-20 h-10">
-                            <p>104,0</p>
+                            <input className="input input-bordered w-20" type="text" />
                         </div>
                         <div className="flex justify-center items-center border w-20 h-10">
-                            <p>108,0</p>
+                            <input className="input input-bordered w-20" type="text" />
                         </div>
                     </div>
                 </div>
