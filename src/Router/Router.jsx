ff236ca7c4a2aa961b32f921e93aa208ba6a0dcb
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-09-02 12:22:05 +0600

    Project Error Solved

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -42,7 +42,7 @@ const router = createBrowserRouter([
             },
             {
                 path: '/profile',
-                element: <UserProfile></UserProfile>
+                element: <PrivateRoute><UserProfile></UserProfile></PrivateRoute>
             }
         ]
 
