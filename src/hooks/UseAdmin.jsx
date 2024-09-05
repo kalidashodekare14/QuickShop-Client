53f25ef1adb80138a839de783615c48b4eacef8c
mrkhan-dev <sk5094625@gmail.com>
2024-09-05 17:21:26 +0600

    setup admin dashboard

--- src/hooks/UseAdmin.jsx
+++ src/hooks/UseAdmin.jsx
@@ -1,14 +1,14 @@
 import {useQuery} from "@tanstack/react-query";
 import useAuth from "./useAuth";
-import useAxiosSecure from "./useAxiosSecure";
+import useAxiosCommon from "./useAxiosCommon";
 
 const UseAdmin = () => {
-  const axiosSecure = useAxiosSecure();
+  const axiosSecure = useAxiosCommon();
   const {user} = useAuth();
   const {data: isAdmin} = useQuery({
     queryKey: [user?.email, "isAdmin"],
     queryFn: async () => {
-      const res = await axiosSecure.get(`allUsers/admin/${user.email}`);
+      const res = await axiosSecure.get(`users/admin/${user.email}`);
       return res.data?.admin;
     },
   });
